bd80e811dcb84643a23e6bde6a6654605fb7bb76
MateoFischer <mateofischer747@gmail.com>
2022-11-13 20:43:39 -0300

    Primera version de la interfaz

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -4,8 +4,16 @@ import { MDCTabBar } from '@material/tab-bar';
 import { MDCTextField } from '@material/textfield';
 import { MDCSelect } from '@material/select';
 import {MDCSnackbar} from '@material/snackbar';
-import ListaPeliculas from '../../dominio/lista-peliculas.mjs';
-import Pelicula from '../../dominio/pelicula.mjs';
+import {MDCMenu} from '@material/menu';
+import {MDCFormField} from '@material/form-field';
+import {MDCRadio} from '@material/radio';
+
+const radio = new MDCRadio(document.querySelector('.mdc-radio'));
+const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+formField.input = radio;
+
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+menu.open = true;
 
 const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
 const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
@@ -13,12 +21,14 @@ const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
 });
 
 
+const textFieldEjemplo = new MDCTextField(document.querySelector('ejemplo'));
+
 const listaPeliculas = new ListaPeliculas();
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 
-const tabBar = new MDCTabBar(document.querySelector(".mdc-tab-bar"));
+const tabBar = new MDCTabBar(document.querySelector(".mdc-tab-bar-equipo"));
 tabBar.listen("MDCTabBar:activated", (activatedEvent) => {
   document.querySelectorAll(".content").forEach((element, index) => {
     if (index === activatedEvent.detail.index) {
@@ -34,82 +44,5 @@ const textFieldYear = new MDCTextField(document.getElementById('year'));
 const selectGenre = new MDCSelect(document.querySelector('.mdc-select'));
 
 const addButton = new MDCRipple(document.getElementById('addButton'));
-addButton.listen('click', () => {
-  let title = textFieldTitle.value;
-  let year = textFieldYear.value;
-  let genre = selectGenre.value;
-  borrarCampos();
-
-  try {
-    let newPelicula = new Pelicula(title, genre, year);
-    listaPeliculas.agregar(newPelicula);
-    const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-    snackbar.labelText = 'Pelicula agregada correctamente';
-    snackbar.open();
-
-  } catch (error) {
-    const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
-    snackbar.labelText = error.message;
-    snackbar.open();
-  } finally {
-    let peliculas = listaPeliculas.getPeliculas();
-    console.log(peliculas);
-    cargarListaPeliculas();
-
-  }
-})
-
-function borrarCampos(){
-  textFieldTitle.value = "";
-  textFieldYear.value = "";
-  selectGenre.value = "";
-}
-
-function cargarListaPeliculas(){
-  let peliculas = listaPeliculas.getPeliculas();
-  let lista = document.getElementById('peliculas');
-  lista.innerHTML = "";
-
-  for(let i = 0; i < peliculas.length; i++){
-    let pelicula = peliculas[i];
-
-    //div principal
-    let fila = document.createElement('div');
-    fila.className = "mdc-card";
-
-    //div primary action
-    let div = document.createElement('div');
-    div.className = "mdc-card__primary-action";
-
-    //info pelicula
-    let informacion = document.createElement("h2")
-    informacion.innerHTML = pelicula.titulo + " (" + pelicula.anio + ")" + " - " + pelicula.genero;
-    div.appendChild(informacion);
-
-    //div mdc-card media square
-    let divMediaSquare = document.createElement('div');
-    divMediaSquare.className = "mdc-card__media mdc-card__media--square";
-    div.appendChild(divMediaSquare);
-
-    //div mdc-card media content
-    let divMediaContent = document.createElement('div');
-    divMediaContent.className = "mdc-card__media-content";
-    divMediaSquare.appendChild(divMediaContent);
-
-    //div mdc ripple
-    let divRipple = document.createElement('div');
-    divRipple.className = "mdc-card__ripple";
-    div.appendChild(divRipple);
-
-    fila.appendChild(div);
-    lista.appendChild(fila);
-
-    let saltoLinea = document.createElement('br');
-    lista.appendChild(saltoLinea);
-    
-    
-
-  }
-}
 
 
